271dd599d7fd1ad6839255cea9e7a8f854f229f0
nicobazzoni <jillianamagsila@Jillians-iMac.fios-router.home>
2021-08-20 16:15:00 -0400

    commit tree 22f61164b0190ccabc4e42adb827e5d77aca9102

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your Circle class here
+
+class Circle {
+    constructor(radius) {
+        this.radius = radius 
+    }
+         
+get diameter() {
+    return this.radius * 2 
+}
+set diameter(newD) {
+    this.radius = newD / 2
+}
+
+
+get circumference() {
+    return Math.PI * this.diameter
+}
+
+set circumference(newCirc) {
+    this.radius = newCirc / (Math.PI * 2)
+}
+
+
+get area() {
+    return Math.PI * this.radius * this.radius
+}
+set area(newArea) {
+    this.radius = Math.sqrt(newArea / Math.PI)
+}
+}
